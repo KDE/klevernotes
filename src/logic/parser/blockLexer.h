77827a4b747035175bbd8a2ddb1e7560f1e59a92
Louis Schul <schul9louis@gmail.com>
2023-08-15 10:17:19 +0200

    Adding block and inline regex

---
+++ src/logic/parser/blockLexer.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <QRegularExpression>
+#include <QString>
+
+class BlockLexer
+{
+public:
+private:
+    QMap<QString, QRegularExpression> preprocessRegex{{"\n", QRegularExpression("\r\n|\r|\u2424")},
+                                                      {"    ", QRegularExpression("\t")},
+                                                      {" ", QRegularExpression("\u00a0")},
+                                                      {"", QRegularExpression("^ +$")}};
+
+    QRegularExpression block_newline = QRegularExpression("^\n+");
+
+    QRegularExpression block_code = QRegularExpression("^( {4}[^\n]+\n*)+");
+
+    QRegularExpression block_fences = QRegularExpression("^ *(\\`{3,}|~{3,})[ \\.]*(\\S+)? *\n([\\s\\S]*?)\n? *\\1 *(?:\n+|$)");
+
+    QRegularExpression block_heading = QRegularExpression("^ *(#{1,6}) *([^\n]+?) *(?:#+ *)?(?:\n+|$)");
+
+    QRegularExpression block_nptable = QRegularExpression("^ *([^|\n ].*\\|.*)\n *([-:]+ *\\|[-| :]*)(?:\n((?:.*[^&gt;\n ].*(?:\n|$))*)\n*|$)");
+
+    QRegularExpression block_hr = QRegularExpression("^ {0,3}((?:- *){3,}|(?:_ *){3,}|(?:\\* *){3,})(?:\n+|$)");
+
+    QRegularExpression block_blockquote = QRegularExpression(
+        "^( {0,3}> ?(([^\n]+(?:\n(?! {0,3}((?:- *){3,}|(?:_ *){3,}|(?:\\* *){3,})(?:\n+|$)| *(#{1,6}) *([^\\n]+?) *(?:#+ *)?(?:\n+|$)|([^\\n]+)\n *(=|-){2,} "
+        "*(?:\n+|$)| "
+        "{0,3}>|<\\/"
+        "?(?:address|article|aside|base|basefont|blockquote|body|caption|center|col|colgroup|dd|details|dialog|dir|div|dl|dt|fieldset|figcaption|figure|footer|"
+        "form|frame|frameset|h[1-6]|head|header|hr|html|iframe|legend|li|link|main|menu|menuitem|meta|nav|noframes|ol|optgroup|option|p|param|section|source|"
+        "summary|table|tbody|td|tfoot|th|thead|title|tr|track|ul)(?: +|\\n|\\/?>)|<(?:script|pre|style|!--))[^\n]+)*)|[^\n]*)(?:\\n|$))+");
+
+    QRegularExpression block_list = QRegularExpression(
+        "^( *)((?:[\\*\\+\\-]|\\d+\\.)) [\\s\\S]+?(?:\n+(?=\\1?(?:(?:- *){3,}|(?:_ *){3,}|(?:\\* *){3,})(?:\n+|$))|\n+(?= "
+        "{0,3}\\[((?!\\s*\\])(?:\\\\[\\[\\]]|[^\\[\\]])+)\\]: *\n? *<!--?([^\\s\\-\\->]+)&gt;?(?:(?: +\n? *| *\n "
+        "*)((?:\"(?:\\\\\"?|[^\"\\\\])*\"|'[^'\n]*(?:\n[^'\n]+)*\n?'|\\([^()]*\\))))? *(?:\n+|$))|\n{2,}(?! )(?!\1(?:[\\*\\+\\-]|\\d+\\.) )\n*|\\s*$)");
+
+    QRegularExpression block_item =
+        QRegularExpression("^( *)((?:[*+-]|\\d+\\.)) [^\\n]*(?:\\n(?!\\1(?:[*+-]|\\d+\\.) )[^\\n]*)*", QRegularExpression::MultilineOption);
+
+    QRegularExpression block_html = QRegularExpression(
+        "^ "
+        "{0,3}(?:&lt;(script|pre|style)[\\s&gt;][\\s\\S]*?(?:&lt;\\/"
+        "\1&gt;[^\n]*\n+|$)|<!--(?!-?>)[\\s\\S]*?-->[^\n]*(\n+|$)|&lt;\\?[\\s\\S]*?\\?&gt;\n*|<!--[A-Z][\\s\\S]*?-->\n*|<!--\\[CDATA\\[[\\s\\S]*?\\]\\]-->\n*|&"
+        "lt;\\/"
+        "?(address|article|aside|base|basefont|blockquote|body|caption|center|col|colgroup|dd|details|dialog|dir|div|dl|dt|fieldset|figcaption|figure|footer|"
+        "form|frame|frameset|h[1-6]|head|header|hr|html|iframe|legend|li|link|main|menu|menuitem|meta|nav|noframes|ol|optgroup|option|p|param|section|source|"
+        "summary|table|tbody|td|tfoot|th|thead|title|tr|track|ul)(?: +|\n|\\/?&gt;)[\\s\\S]*?(?:\n{2,}|$)|&lt;(?!script|pre|style)([a-z][\\w-]*)(?: "
+        "+[a-zA-Z:_][\\w.:-]*(?: *= *\"[^\"\n]*\"| *= *'[^'\n]*'| *= *[^\\s\"'=&lt;&gt;`]+)?)*? "
+        "*\\/?&gt;(?=\\h*\n)[\\s\\S]*?(?:\n{2,}|$)|&lt;\\/(?!script|pre|style)[a-z][\\w-]*\\s*&gt;(?=\\h*\n)[\\s\\S]*?(?:\n{2,}|$))",
+        QRegularExpression::CaseInsensitiveOption);
+
+    QRegularExpression block_def = QRegularExpression(
+        "^ {0,3}\\[((?!\\s*\\])(?:\\\\[\\[\\]]|[^\\[\\]])+)\\]: *\\n? *<?([^\\s>]+)>?(?:(?: +\\n? *| *\\n "
+        "*)((?:\"(?:\\\\\"?|[^\"\\\\])*\"|'[^'\n]*(?:\n[^'\n]+)*\n?'|\\([^()]*\\))))? *(?:\\n+|$)");
+
+    QRegularExpression block_table = QRegularExpression("^ *\\|(.+)\n *\\|?( *[-:]+[-| :]*)(?:\n((?: *[^&gt;\n ].*(?:\n|$))*)\n*|$)");
+
+    QRegularExpression block_lheading = QRegularExpression("^([^\n]+)\n *(=|-){2,} *(?:\n+|$)");
+
+    QRegularExpression block_paragraph = QRegularExpression(
+        "^([^\\n]+(?:\\n(?! *(`{3,}|~{3,})[ \\.]*(\\S+)? *\\n([\\s\\S]*?)\\n? *\\2 *(?:\\n+|$)|( *)((?:[*+-]|\\d+\\.)) [\\s\\S]+?(?:\\n+(?=\\3?(?:(?:- "
+        "*){3,}|(?:_ *){3,}|(?:\\* *){3,})(?:\\n+|$))|\\n+(?= {0,3}\\[((?!\\s*\\])(?:\\\\[\\[\\]]|[^\\[\\]])+)\\]: *\\n? *<?([^\\s>]+)>?(?:(?: +\\n? *| *\\n "
+        "*)((?:\"(?:\\\\\"?|[^\"\\\\])*\"|'[^'\\n]*(?:\\n[^'\\n]+)*\\n?'|\\([^()]*\\))))? *(?:\\n+|$))|\\n{2,}(?! )(?!\\1(?:[*+-]|\\d+\\.) )\\n*|\\s*$)| "
+        "{0,3}((?:- *){3,}|(?:_ *){3,}|(?:\\* *){3,})(?:\\n+|$)| *(#{1,6}) *([^\\n]+?) *(?:#+ *)?(?:\\n+|$)|([^\\n]+)\\n *(=|-){2,} *(?:\\n+|$)| "
+        "{0,3}>|<\\/"
+        "?(?:address|article|aside|base|basefont|blockquote|body|caption|center|col|colgroup|dd|details|dialog|dir|div|dl|dt|fieldset|figcaption|figure|footer|"
+        "form|frame|frameset|h[1-6]|head|header|hr|html|iframe|legend|li|link|main|menu|menuitem|meta|nav|noframes|ol|optgroup|option|p|param|section|source|"
+        "summary|table|tbody|td|tfoot|th|thead|title|tr|track|ul)(?: +|\\n|\\/?>)|<(?:script|pre|style|!--))[^\\n]+)*)");
+
+    QRegularExpression block_text = QRegularExpression("^[^\n]+");
+};
---
+++ src/logic/parser/inlineLexer.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <QRegularExpression>
+
+class InlineLexer
+{
+public:
+private:
+    QRegularExpression inline_escape = QRegularExpression("^\\\\([!\"#$%&'()*+,\\-.\\/:;<=>?@\\[\\]\\\\^_`{|}~~|])");
+
+    QRegularExpression inline_autolink = QRegularExpression(
+        "^<([a-zA-Z][a-zA-Z0-9+.-]{1,31}:[^\\s\\x00-\\x1f<>]*|[a-zA-Z0-9.!#$%&'*+/"
+        "=?_`{|}~-]+(@)[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+(?![-_]))>");
+
+    QRegularExpression inline_url = QRegularExpression(
+        "^((?:ftp|https?):\\/\\/|www\\.)(?:[a-zA-Z0-9\\-]+\\.?)+[^\\s<]*|^[a-zA-Z0-9.!#$%&'*+/"
+        "=?_`{|}~-]+(@)[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+(?![-_])");
+
+    QRegularExpression inline_tag = QRegularExpression(
+        "^<!--(?!-?>)[\\s\\S]*?-->|^<\\/"
+        "[a-zA-Z][\\w:-]*\\s*>|^<[a-zA-Z][\\w-]*(?:\\s+[a-zA-Z:_][\\w.:-]*(?:\\s*=\\s*\"[^\"]*\"|\\s*=\\s*'[^']*'|\\s*=\\s*[^\\s\"'=<>`]+)?)*?\\s*\\/"
+        ">|^<\\?[\\s\\S]*?\\?>|^<!\\[CDATA\\[[\\s\\S]*?\\]\\]>");
+
+    QRegularExpression inline_link = QRegularExpression(
+        "^!?\\[((?:\\[[^\\[\\]]*\\]|\\\\[\\[\\]]?|`[^`]*`|[^\\[\\]\\\\])*?)\\]\\(\\s*(<(?:\\\\[<>]?|[^\\s<>\\\\])*>|(?:\\\\[()]?|\\([^\\s\\x00-\\x1f()\\\\]*\\)"
+        "|[^\\s\\x00-\\x1f()\\\\])*?)(?:\\s+(\"(?:\\\\\"?|[^\"\\\\])*\"|'(?:\\\\'?|[^'\\\\])*'|\\((?:\\\\\\)?|[^)\\\\])*\\)))?\\s*\\)");
+
+    QRegularExpression inline_reflink =
+        QRegularExpression("^!?\\[((?:\\[[^\\[\\]]*\\]|\\\\[\\[\\]]?|`[^`]*`|[^\\[\\]\\\\])*?)\\]\\[(?!\\s*\\])((?:\\[\\[\\]]?|[^\\[\\]\\\\])+)\\]");
+
+    QRegularExpression inline_nolink = QRegularExpression("^!?\\[(?!\\s*\\])((?:\\[[^\\[\\]]*\\]|\\\\[\\[\\]]|[^\\[\\]])*)\\](?:\\[\\])?");
+
+    QRegularExpression inline_strong =
+        QRegularExpression("^__([^\\s][\\s\\S]*?[^\\s])__(?!_)|^\\*\\*([^\\s][\\s\\S]*?[^\\s])\\*\\*(?!\\*)|^__([^\\s])__(?!_)|^\\*\\*([^\\s])\\*\\*(?!\\*)");
+
+    QRegularExpression inline_em = QRegularExpression(
+        "^_([^\\s][\\s\\S]*?[^\\s_])_(?!_)|^_([^\\s_][\\s\\S]*?[^\\s])_(?!_)|^\\*([^\\s][\\s\\S]*?[^\\s*])\\*(?!\\*)|^\\*([^\\s*][\\s\\S]*?[^\\s])\\*(?!\\*)|^_"
+        "([^\\s_])_(?!_)|^\\*([^\\s*])\\*(?!\\*)");
+
+    QRegularExpression inline_code = QRegularExpression("^(\\`{1,})\\s*([\\s\\S]*?[^`]?)\\s*\\1(?!`)");
+
+    QRegularExpression inline_br = QRegularExpression("^ {2,}\n(?!\\s*$)");
+
+    QRegularExpression inline_del = QRegularExpression("^~~(?=\\S)([\\s\\S]*?\\S)~~");
+
+    QRegularExpression inline_text =
+        QRegularExpression("^[\\s\\S]+?(?=[\\\\<!\\[`*~]|https?:\\/\\/|ftp:\\/\\/|www\\.|[a-zA-Z0-9.!#$%&'*+/=?_`{\\|}~-]+@|\b_| {2,}\n|$)");
+
+    QRegularExpression inline_backPedal = QRegularExpression("(?:[^?!.,:;*_~()&]+|\\([^)]*\\)|&(?![a-zA-Z0-9]+;$)|[?!.,:;*_~)]+(?!$))+");
+};
