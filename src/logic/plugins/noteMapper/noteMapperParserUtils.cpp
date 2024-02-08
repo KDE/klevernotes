02a10cf2660d3e66d687622393bc412514ab58ea
Louis Schul <schul9louis@gmail.com>
2024-02-08 09:44:26 +0100

    Fix noteMapper header updates

--- src/logic/plugins/noteMapper/noteMapperParserUtils.cpp
+++ src/logic/plugins/noteMapper/noteMapperParserUtils.cpp
@@ -121,7 +121,7 @@ void NoteMapperParserUtils::postTok()
     }
     m_previousLinkedNotesInfos = m_linkedNotesInfos;
 
-    if (m_linkedNotesChanged || !m_previousNoteHeaders.isEmpty()) { // The previous is not empty, some headers are no longer there
+    if (m_noteHeadersChanged || !m_previousNoteHeaders.isEmpty()) { // The previous is not empty, some headers are no longer there
         m_emptyHeadersSent = false;
         Q_EMIT m_parser->noteHeadersSent(m_mapperNotePath, m_noteHeaders.values());
     } else if (m_noteHeaders.isEmpty() && !m_emptyHeadersSent) {
