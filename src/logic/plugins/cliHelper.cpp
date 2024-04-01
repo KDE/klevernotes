f992db692e6082f2b5081ea556964959b6df10f8
Louis Schul <schul9louis@gmail.com>
2024-04-01 21:59:19 +0200

    Fix execution of external program with flatpak
    The code highlighting and PUML now work on flatpak

--- src/logic/plugins/cliHelper.cpp
+++ src/logic/plugins/cliHelper.cpp
@@ -3,25 +3,42 @@
 
 #include "cliHelper.h"
 
+#include <KSandbox>
 #include <QProcess>
 #include <QStandardPaths>
 
 QString CLIHelper::findExecutable(const QString &command)
 {
+    if (KSandbox::isInside()) {
+        QProcess process;
+        process.setProgram(QStringLiteral("which"));
+        process.setArguments(QStringList() << command);
+        KSandbox::startHostProcess(process);
+
+        if (!process.waitForStarted()) {
+            return {};
+        }
+
+        process.waitForFinished(1000);
+
+        return process.exitCode() == 0 ? QString::fromUtf8(process.readAllStandardOutput()).trimmed() : QLatin1String();
+    }
+
     return QStandardPaths::findExecutable(command);
 }
 
 QString CLIHelper::execCommand(const QString &input)
 {
-    QProcess process;
-
     const QString sh = findExecutable(QStringLiteral("sh"));
     if (sh.isEmpty()) {
         return {};
     }
 
+    QProcess process;
     // We let sh handle the pipes and all the args, easier than using multiple QProcess
-    process.start(sh, QStringList() << QStringLiteral("-c") << input);
+    process.setProgram(sh);
+    process.setArguments(QStringList() << QStringLiteral("-c") << input);
+    KSandbox::startHostProcess(process);
 
     if (!process.waitForStarted()) {
         return {};
