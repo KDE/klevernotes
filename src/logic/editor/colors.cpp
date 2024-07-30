449c8731680c321ef3aac919b9d6abd1422f4f48
Louis Schul <schul9louis@gmail.com>
2024-07-30 09:23:55 +0200

    Add basic highlight in editor

---
+++ src/logic/editor/colors.cpp
@@ -0,0 +1,16 @@
+/*
+    SPDX-License-Identifier: GPL-2.0-or-later
+    SPDX-FileCopyrightText: 2024 Louis Schul <schul9louis@gmail.com>
+*/
+
+#include "colors.hpp"
+
+namespace MdEditor
+{
+bool operator!=(const Colors &c1, const Colors &c2)
+{
+    return (c1.enabled != c2.enabled || c1.textColor != c2.textColor || c1.linkColor != c2.linkColor || c1.mathColor != c2.mathColor
+            || c1.specialColor != c2.specialColor || c1.titleColor != c2.titleColor || c1.highlightColor != c2.highlightColor || c1.codeColor != c2.codeColor
+            || c1.codeBgColor != c2.codeBgColor);
+}
+}
---
+++ src/logic/editor/editorHighlighter.hpp
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// SPDX-FileCopyrightText: 2024 Louis Schul <schul9louis@gmail.com>
+
+#pragma once
+
+// KleverNotes include
+#include "logic/editor/colors.hpp"
+#include "logic/parser/plugins/emoji/emojiPlugin.hpp"
+
+// md4qt include.
+#define MD4QT_QT_SUPPORT
+#include "logic/parser/md4qt/poscache.hpp"
+#include "logic/parser/md4qt/traits.hpp"
+
+// Qt include.
+#include <QColor>
+#include <QScopedPointer>
+
+// C++ include.
+#include <vector>
+
+namespace MdEditor
+{
+class EditorHandler;
+struct EditorHighlighterPrivate;
+
+//! Markdown syntax highlighter.
+class EditorHighlighter : public MD::PosCache<MD::QStringTrait>
+{
+public:
+    explicit EditorHighlighter(EditorHandler *editor);
+    ~EditorHighlighter() override;
+
+    void highlight(std::shared_ptr<MD::Document<MD::QStringTrait>> doc, const Colors &colors);
+    void setFont(const QFont &f);
+    void clearHighlighting();
+
+    void addExtendedSyntax(const long long int opts, const QStringList &info);
+
+protected:
+    void onReferenceLink(MD::Link<MD::QStringTrait> *l) override;
+    void onText(MD::Text<MD::QStringTrait> *t) override;
+    void onMath(MD::Math<MD::QStringTrait> *m) override;
+    void onHeading(MD::Heading<MD::QStringTrait> *h) override;
+    void onCode(MD::Code<MD::QStringTrait> *c) override;
+    void onInlineCode(MD::Code<MD::QStringTrait> *c) override;
+    void onBlockquote(MD::Blockquote<MD::QStringTrait> *b) override;
+    void onTable(MD::Table<MD::QStringTrait> *t) override;
+    void onRawHtml(MD::RawHtml<MD::QStringTrait> *h) override;
+    void onHorizontalLine(MD::HorizontalLine<MD::QStringTrait> *l) override;
+    void onLink(MD::Link<MD::QStringTrait> *l) override;
+    void onImage(MD::Image<MD::QStringTrait> *i) override;
+    void onFootnoteRef(MD::FootnoteRef<MD::QStringTrait> *ref) override;
+    void onFootnote(MD::Footnote<MD::QStringTrait> *f) override;
+    void onListItem(MD::ListItem<MD::QStringTrait> *l, bool first) override;
+    void onUserDefined(MD::Item<MD::QStringTrait> *item) override;
+
+private:
+    void onItemWithOpts(MD::ItemWithOpts<MD::QStringTrait> *i);
+
+    void onEmoji(EmojiPlugin::EmojiItem *e);
+
+private:
+    Q_DISABLE_COPY(EditorHighlighter)
+
+    QScopedPointer<EditorHighlighterPrivate> d;
+}; // !EditorHighlighter
+
+} // !namespace MdEditor
