771a827da58150bb47a9dabb8780b40be8fbb1e7
Louis Schul <schul9louis@gmail.com>
2024-10-08 10:53:05 +0200

    Bump version

--- src/main.cpp
+++ src/main.cpp
@@ -57,7 +57,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
         // A displayable program name string.
         i18nc("@title", "KleverNotes"),
         // The program version string.
-        QStringLiteral("1.1.0"),
+        QStringLiteral("1.1.1"),
         // Short description of what the app does.
         i18n("Note taking and management application"),
         // The license this code is released under.
